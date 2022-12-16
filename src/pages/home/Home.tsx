88aa0057f523724b475538c46c7a7ff72983aa90
YJouhar47 <youssef.jouhar1998@hotmail.com>
2022-12-16 16:01:53 +0100

    Css updated

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,21 @@
 import ProgressBar from "@ramonak/react-progress-bar";
+import { Link } from "react-router-dom";
 import Layout from "../../Components/Layout/Layout";
 import styles from './Home.module.css'
 const Aboutme = () => {
     return (
         <Layout>
-            <div className={styles.wrapper}>
-                <div className={styles.header}>
-                    <h1>I am Youssef Jouhar</h1>
-                    <h2>I am 22 Years old</h2>
-                    <h3>Studying at Artesis Plantijn Antwerpen Hogeschool</h3>
+            <div className={styles.header}>
+                <h1>I am Youssef Jouhar</h1>
+                <h2>I am 22 Years old</h2>
+                <h3>Studying at Artesis Plantijn Antwerpen Hogeschool</h3>
+                <Link to="/portofolio"><button className={styles.button}>Go to portofolio</button></Link>
+            </div>
+            <div className={styles.skills}>
+                <div className={styles.skillstext}>
+                    <h1>SKILLS &rarr;</h1>
                 </div>
-                <div className={styles.img}>
-                    <img src="https://i.fokzine.net/upload/21/03/210307_1_daily255_01.jpg" alt="foto"/>
-                </div>
-                <div className={styles.bigtitle}>
-                    <h1>SKILLS</h1>
-                </div>
-                <div className={styles.progress}>
-                    <div className={styles.bar}>
+                <div className={styles.skillsprogress}>
                     <ProgressBar customLabel="React (82%)" labelAlignment="center" completed={82} width='100%' bgColor="black" margin="5px" />
                     <ProgressBar customLabel="C# (95%)" labelAlignment="center" completed={95} width='100%' bgColor="black" margin="5px" />
                     <ProgressBar customLabel="Typescript (75%)" labelAlignment="center" completed={75} width='100%' bgColor="black" margin="5px" />
@@ -25,6 +23,8 @@ const Aboutme = () => {
                     <ProgressBar customLabel="C++ (80%)" labelAlignment="center" completed={80} width='100%' bgColor="black" margin="5px" />
                     <ProgressBar customLabel="SQL (55%)" labelAlignment="center" completed={55} width='100%' bgColor="black" margin="5px" />
                 </div>
+                <div className={styles.img}>
+                    <img src="https://i.guim.co.uk/img/media/583b1b167c0b51719de576054d524326e577b21f/0_206_4488_2693/master/4488.jpg?width=1200&quality=85&auto=format&fit=max&s=634e0f9f6e23310f5e9432207b892432" alt="foto" />
                 </div>
             </div>
         </Layout>
